6474c6cba54f83c0b951fd23e682c20fb6b092bd
Saint <sikim0721@gmail.com>
2024-02-16 16:09:24 +0900

    글 작성기능 구현

---
+++ router/write.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+
+// MongoDB 연결
+let connectDB = require('./db.js')
+const { ObjectId } = require('mongodb')
+let db 
+connectDB.then((client) => {
+  db = client.db('forum')
+}).catch((err) => {
+  console.error(err)
+})
+
+// 라우터 설정
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended : true }))
+
+router.get('/', async (req, res) => {
+  try {
+    let result = await db.collection('post').find().toArray()
+    res.render('write.ejs', { list : result })
+  } catch(err) {
+    console.error(err)
+  }
+})
+
+router.post('/', async (req, res) => {
+  await db.collection('post').insertOne({ title : req.body.title, content : req.body.content })
+  res.redirect('/list')
+})
+
+module.exports = router
